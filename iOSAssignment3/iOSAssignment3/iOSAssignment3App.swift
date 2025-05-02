d9b5ace1da32a7913183a5729c33765bae68c191
Ronin Campbell <ronincampbell3@gmail.com>
2025-05-02 13:52:47 +1000

    Initial Commit

---
+++ iOSAssignment3/iOSAssignment3/iOSAssignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAssignment3App.swift
+//  iOSAssignment3
+//
+//  Created by Luna Campbell on 2/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSAssignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
