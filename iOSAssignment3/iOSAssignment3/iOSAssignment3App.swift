e69d194db4b6b873bde04ccff390892e99e4b9cd
Bradley Hoy <bradley.hoy@student.uts.edu.au>
2025-05-09 14:13:39 +1000

    xcode 14 test

--- iOSAssignment3/iOSAssignment3/iOSAssignment3App.swift
+++ iOSAssignment3/iOSAssignment3/iOSAssignment3App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct iOSAssignment3App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PokemonBattleCalculatorView()
         }
     }
 }
